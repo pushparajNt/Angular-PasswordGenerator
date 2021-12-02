885b69746fe32938f877e774dbe389d9a41118f5
Pushparaj <pushparajcsegce@gmail.com>
2021-12-02 22:04:31 +0530

    event and property binding

--- PasswordGenerator/src/app/app.component.ts
+++ PasswordGenerator/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PasswordGenerator';
+  
+  title = '';
+  generate()
+  {
+    console.log("generate clicked");
+    this.title="Pushparaj";
+  }
+  
 }
