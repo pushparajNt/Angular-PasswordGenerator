5651a2bc15f5a3a1e682fa5b54b4d4f0cd82b462
Pushparaj <pushparajcsegce@gmail.com>
2021-12-03 09:34:53 +0530

    event bindings added

--- PasswordGenerator/src/app/app.component.ts
+++ PasswordGenerator/src/app/app.component.ts
@@ -8,10 +8,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   title = '';
+  checkUseLetters=false;
+  checkUseNumbers=false;
+  checkUseSymbols=false;
+  passLength=0;
   generate()
   {
-    console.log("generate clicked");
     this.title="Pushparaj";
+    console.log(`Generate password with the following:
+                 includes letters:${this.checkUseLetters}
+                 Includes Numbers:${this.checkUseNumbers}
+                 Includes Symbols:${this.checkUseSymbols}`);
   }
-  
+
+  onChangePassLength(value:string)
+  {
+   
+    if(!isNaN(Number(value)))
+    {
+        this.passLength=Number(value);
+        console.log(this.passLength);
+        console.log(typeof(this.passLength));
+    }
+
+  }
+
+  useLetters()
+  {
+    this.checkUseLetters=!this.checkUseLetters;
+  }
+  useNumbers()
+  {
+    this.checkUseNumbers=!this.checkUseNumbers;
+  }
+  useSymbols()
+  {
+    this.checkUseSymbols=!this.checkUseSymbols;
+  }
 }
