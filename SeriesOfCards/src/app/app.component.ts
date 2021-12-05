10c7be52abdd08924311355879c625ba92fd4191
Pushparaj <pushparajcsegce@gmail.com>
2021-12-05 20:56:19 +0530

    parent child communication

--- SeriesOfCards/src/app/app.component.ts
+++ SeriesOfCards/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SeriesOfCards';
+ 
+  posts=[
+    {
+     title:'Neat Tree',
+     imageUrl:'assets/tree.jpeg',
+     username:'nature',
+     content:'I saw this neat tree today' 
+    },
+     {
+     title:'snowy mountain',
+     imageUrl:'assets/mountain.jpeg',
+     username:'mountain lover',
+     content:'Here is a picture of a snowy mountain' 
+    },
+     {
+     title:'mountain biking',
+     imageUrl:'assets/biking.jpeg',
+     username:'biking',
+     content:'I did some biking today' 
+    }
+    
+  ]
 }
