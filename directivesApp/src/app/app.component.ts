cacece5e2d3c9d425e9887c04c9966d0d54a9d23
Pushparaj <pushparajcsegce@gmail.com>
2021-12-13 21:34:11 +0530

    ngclass directive

--- directivesApp/src/app/app.component.ts
+++ directivesApp/src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivesApp';
+
+  currentPage=0;
+
+  images=[
+    {
+     title: 'At the beach',
+     url:'https://images.unsplash.com/flagged/photo-1575388105878-0ff8d276d368?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
+    },
+     {
+     title: 'At the forest',
+     url:'https://images.unsplash.com/photo-1473448912268-2022ce9509d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1141&q=80'
+    },
+     {
+     title: 'At the beach',
+     url:'https://images.unsplash.com/photo-1600270717422-06fc3227331b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+    },
+      {
+     title: 'At the beach',
+     url:'https://images.unsplash.com/photo-1437964706703-40b90bdf563b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+    }
+  ]
 }
